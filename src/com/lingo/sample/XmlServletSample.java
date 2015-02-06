2d8080d7b75cbfc7b631df08681022fd87dac540
berryfine <gouyf02@gmail.com>
2015-02-05 23:20:23 -0600

    adding session part to remember the parameter; but it only works in the same brower

--- src/com/lingo/sample/XmlServletSample.java
+++ src/com/lingo/sample/XmlServletSample.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -15,7 +16,12 @@ public class XmlServletSample extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String userName = request.getParameter("userName");
-        out.write("hello "+userName +" welcome to getMethod");
+        HttpSession session = request.getSession();
+        if(userName!=null && userName!=""){
+            session.setAttribute("savedUserName",userName);
+        }
+        out.println("hello "+userName +" welcome to getMethod");
+        out.println("Hello, I have been remembered. "+session.getAttribute("savedUserName"));
         out.flush();
     }
 
@@ -24,8 +30,15 @@ public class XmlServletSample extends HttpServlet {
         PrintWriter out = response.getWriter();
         String userName = request.getParameter("userName");
         String fullName = request.getParameter("fullName");
-        out.write("hello "+userName +", welcome to PostMethod.\n");
+        out.write("hello " + userName + ", welcome to PostMethod.");
         out.write("your full name is "+fullName);
+        String professor = request.getParameter("prof");
+        out.write("\n you are a "+professor);
+        String[] locations  = request.getParameterValues("location");
+        out.write("\n you have lived in:");
+        for(String s:locations){
+            out.write(s+",\t");
+        }
         out.flush();
     }
 }
